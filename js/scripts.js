7a0914e43d571576afaacb6fbaa472416924327e
MarielHamson <mariel.hamson@gmail.com>
2020-05-20 13:26:27 -0700

    refactor code again

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,17 @@ const robotese = function(translation) {
 
   for (let index = 0; index <= translation; index += 1) {
 
-  const str = index.toString(); 
+      const str = index.toString();
 
-  if (str.includes("3") === true) {
-    wordsArray.push(" Won't you be my neighbor?");
-  } else if (str.includes("2") === true) {
-    wordsArray.push(" Boop!");
-  } else if (str.includes("1") === true) {
-    wordsArray.push(" Beep!");
-  } else { 
-    wordsArray.push(" " + str);
-  }
+      if (str.includes("3") === true) {
+          wordsArray.push(" Won't you be my neighbor?");
+      } else if (str.includes("2") === true) {
+          wordsArray.push(" Boop!");
+      } else if (str.includes("1") === true) {
+          wordsArray.push(" Beep!");
+      } else {
+          wordsArray.push(" " + str);
+      }
 
   };
 
@@ -25,20 +25,21 @@ const robotese = function(translation) {
 };
 
 //User Logic
+
 $(document).ready(function() {
   $(".form-inline").submit(function(event) {
-    firstName = $("input#name").val();
+      firstName = $("input#name").val();
 
-    $("#robot-lang").show();
+      $("#robot-lang").show();
 
-    event.preventDefault();
+      event.preventDefault();
   });
 
- $("form#robot-lang").submit(function() {
- event.preventDefault();
-  const translation = $("#words").val();
-const result = robotese(translation);
-$(".translation").text("Dearest " + firstName + ": " + result );  
-$("#result").show();
- });
+  $("form#robot-lang").submit(function() {
+      event.preventDefault();
+      const translation = $("#words").val();
+      const result = robotese(translation);
+      $(".translation").text("Dearest " + firstName + ": " + result);
+      $("#result").show();
+  });
 });
